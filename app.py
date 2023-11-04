4216542becaab98f97b001e87555725fb655cdb0
Alissa Chimienti <alissachimienti@gmail.com>
2023-11-03 19:13:33 -0500

    added framework

--- app.py
+++ app.py
@@ -6,6 +6,14 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/maps')
+def maps():
+    return render_template('maps.html')
+
+@app.route('/game')
+def game():
+    return render_template('game.html')
+
 
 if __name__ == '__main__':
    app.run(debug=True, host='0.0.0.0', port=80)

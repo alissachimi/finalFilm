31c761d58f2d81d1bdc3d1979d9085fe584537ab
meganmurphh <mlm4hb@umsystem.edu>
2023-11-04 15:29:29 -0500

    updated the home.js, home.html and home.css

--- static/JS/home.js
+++ static/JS/home.js
@@ -1,6 +1,11 @@
-function initAutoComplete(){
-    var input = document.getElementById("places-search");
-    var autocomplete = new google.maps.places.Autocomplete(input);
-}
-
-google.maps.event.addDomListener(window, 'load', initAutoComplete);
+function initAutoComplete() {
+    const input = document.getElementById("places-search");
+    var autocomplete = new google.maps.places.SearchBox(input);
+  
+    google.maps.event.addListener(autocomplete, 'place_changed', function(){
+      var place = autocomplete.getPlaces();
+    });
+  };
+  
+  google.maps.event.addDomListener(window, 'load', initAutoComplete);
+  

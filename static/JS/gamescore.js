7146a0d3c4100d258c980e99d6b73e900cbf67af
Alyssa <alyssarschaefer@gmail.com>
2023-11-04 15:59:38 -0500

    still working on score functions

--- static/JS/gamescore.js
+++ static/JS/gamescore.js
@@ -1,4 +1,4 @@
-/* GAME SCORE */
+///////////////////* GAME SCORE *///////////////////
 
 let score = 0; //initialize game score
 
@@ -17,12 +17,34 @@ function addCorrectPoints(score){
     score = score + 50;
 }
 
-
+//tracks value of text 'answer' form
 var input = document.getElementById("guess");
+
+attempt = 1;
+
+//called when the user presses the ENTER key
+input.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+        correct = isGuessCorrect(input);
+        if (correct = false){
+            attempt++;
+            subtractBadGuessPoints(score);
+        }
+
+        if (correct = true){
+            addCorrectPoints(score);
+        }
+    }
+});
 
 /*This function determines if the user's guess is correct 
 or not and stores this as a boolean variable */
-function isGuessCorrect(){
-    correct = true;
-    return correct;
+function isGuessCorrect(input){
+
+    //return true if input matches movie title
+    if (input.toLowerCase() === movie.movie.toLowerCase()) {
+        return true;
+    }
+    //return false otherwise
+    return false;
 }
